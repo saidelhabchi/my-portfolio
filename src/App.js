987c9a7606bf7d21e29a83119c6a6fcbc5c627e7
saidelhabchi <elhabchisaid40@gmail.com>
2023-01-08 22:33:55 +0100

    latest version

--- src/App.js
+++ src/App.js
@@ -3,25 +3,49 @@ import About from "./About";
 import Competences from "./Competences";
 import Contacts from "./Contacts";
 import Education from "./Education";
-import Navbar from "./Navbar";
 import Projects from "./Projects";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import logo from './logo.PNG';
+import React, { useState } from "react";
+
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
-    <div className="App container">
-      <Navbar />
+    <div className={ darkMode ? "dark-mode" : "light-mode" }>
+      <div className="container">
       
-      <About />
+      <div className="navbar">
+          <img src={logo} alt="logo" className={"logo rounded "}/>
+          {/** 
+          <h3 className={darkMode ? "dark-mode" : "light-mode"}>logo</h3>
+          */}
+          <div className="d-flex justify-content-between fs-3 mt-5">
+              <div className={darkMode ? "dark-mode" : "light-mode"}><BsFillMoonStarsFill onClick={()=>{
+                setDarkMode(!darkMode)
+              }}/></div>
+              <a href="Said Elhabchi CV.pdf"
+              download={"Said Elhabchi CV.pdf"}>
+                  <button type="button" class={darkMode ? "btn btn-light mx-4" : "btn btn-dark mx-4"}>Resume</button>
+              </a>                
+          </div>
+      </div>
+
+      
+      <About mode={darkMode ? "dark-mode" : "light-mode"}/>
       <hr/>
-      <Education />
+      <Education mode={darkMode ? "dark-mode" : "light-mode"}/>
       <hr/>
-      <Competences />
+      <Competences mode={darkMode ? "dark-mode" : "light-mode"}/>
       <hr/>
-      <Projects />
+      <Projects mode={darkMode ? "dark-mode" : "light-mode"}/>
       <hr/>
-      <Contacts />
+      <Contacts mode={darkMode ? "dark-mode" : "light-mode"}/>
+      </div>
+
     </div>
   );
 }
+
 
 export default App;
--- src/Contacts.js
+++ src/Contacts.js
@@ -1,21 +1,23 @@
-const Contacts = () => {
+const Contacts = (props) => {
     return ( 
         <div className="footer container">
-            <h1 className=" text-center">Contacts</h1>
+            <div className="text-center">
+                <h1 className={props.mode}>Contacts</h1>
+            </div>
             <div className="row align-items-center">
             <div className="col">
-            <h3>Social Media</h3>
-            <ul>
-                <li><a href="#">Instagram</a></li>
-                <li><a href="#">LinkedIn</a></li>
-                <li><a href="#">Github</a></li>
+            <h3 className={props.mode}>Social Media</h3>
+            <ul className="d-decoration">
+                <li><a className={props.mode} href="https://www.instagram.com/saidelhabchii/">Instagram</a></li>
+                <li><a className={props.mode} href="https://www.linkedin.com/in/said-elhabchi-7a3a53234/">LinkedIn</a></li>
+                <li><a className={props.mode} href="https://github.com/saidelhabchi">Github</a></li>
             </ul>
             </div>
             <div className="col">
-            <h3>Freelance platformes</h3>
-            <ul>
-                <li><a href="#">UpWork</a></li>
-                <li><a href="#">Fiverr</a></li>
+            <h3 className={props.mode}>Freelance platformes</h3>
+            <ul className="d-decoration">
+                <li><a className={props.mode} href="https://www.upwork.com/freelancers/~011e05e472f0250ddb?viewMode=1">UpWork</a></li>
+                <li><a className={props.mode} href="https://www.freelancer.com/u/saidelhabchi31">Freelancer</a></li>
             </ul>
             </div>
             
